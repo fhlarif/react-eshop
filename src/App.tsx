dc11bb9866e45b6e46b670441c5ac43297bb5527
fathularif <fathulcode@gmail.com>
2022-11-09 16:00:18 +0800

    setup folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="text-center">Hellow There</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
